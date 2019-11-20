4d5bd0d980ca53ba67bd955805ab52c2f0c1f4f8
tangkun <tangkun4379@haixue.com>
2019-11-20 11:07:36 +0800

    11月20提交前端相关知识

---
+++ html_css_js_learn/js_learn.js
@@ -0,0 +1,4 @@
+/*
+* 参考资料：https://www.w3school.com.cn/js/index.asp
+* window对象：https://www.w3school.com.cn/js/js_window.asp
+* */
---
+++ pytest_learn/base_pytest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__author__ = 'caden'
+"""
+description:pytest基本信息学习
+"""
